654931e43d7f67b01d0e113cbd5e31a3f579aeff
NazariiKoropis”git config --global user.name NazariiKoropis” <nazarkoropisprogramming@gmail.comgit config --global user.email nazarkoropisprogramming@gmail.com>
2025-09-03 15:02:11 +0300

    “Lab2”

---
+++ PR2/PR2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main(){
+
+	int a;
+
+	//comit lab21
+	std::cout << "Enter an integer: ";
+	std::cin >> a;
+
+	double z1 = std::pow(((1+a+std::pow(a,2))/2*a+std::pow(a,2) + 2 - (1 - a + std::pow(a, 2)) / 2 * a - std::pow(a, 2) ), -1) * (5 - 2*std::pow(a,2));
+
+	double z2 = (4-std::pow(a,2)) /2;
+
+	std::cout << "z1 = " << z1 << std::endl;
+	std::cout << "z2 = " << z2 << std::endl;
+
+	return 0;
+}
+
